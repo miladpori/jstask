cb42bbed1d839a2df9b0a3f11fbb370a3e458462
miladpqb <milimaximus1374@gmail.com>
2023-03-18 16:20:50 +0400

    secont up

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let newNumber = 1;
 if (addQuestion) {
 
     let tableAbjust = document.querySelector("#aqf_tab")
-    
+
 
     addQuestion.addEventListener('click', function (e) {
         e.preventDefault();
@@ -23,164 +23,465 @@ if (addQuestion) {
     })
 }
 
-
 let qForm = document.querySelector('#q_form')
-let answerList = []
 
-let count = 0;
+
+let obj = {
+    "id": "111111111111",
+    "created_time": "time not set",
+    "items": [
+        {
+            "ques_id": null,
+            "show": true,
+            "title": null,
+            "type": "radio",
+            "answers": [
+                {
+                    "ans_id": '',
+                    "title": '',
+                    "child": [],
+                    "default": [],
+                    "descrip": false,
+                    "desctext": "",
+                    "name": '',
+                }
+            ],
+            "ques_can_select": [
+
+            ]
+
+        },
+    ]
+}
+
+
+idCount = 0
 qForm.addEventListener('submit', function (e) {
     e.preventDefault();
+
+    // delete(obj);
 
 
     let aqInput = document.querySelector('.aq-input')
     let Qform = document.querySelector('#q_form')
+    let aqTitles = document.querySelectorAll('.aq-titles')
+    let isValid = true;
 
+
+    function ret() {
+        return
+    }
     if (aqInput.value !== '') {
+        let counter = 0;
+        inputsValid(aqTitles)
+        function inputsValid(items) {
+            items.forEach(item => {
+                if (item.value !== '') {
+                    counter++;
+                }
+            })
+        }
 
-        let data = new FormData(Qform)
-        let blosh = document.querySelectorAll('.blosh')
-        let aqTitles = document.querySelectorAll('.aq-titles')
-        count++
 
-        let answerOutput = ''
-        let textareaOutpot = ''
 
-        blosh.forEach(item => {
-            let aqTiltle = item.querySelector('.aq-titles')
-            let aqCheckbox = item.querySelector('.checkbox')
 
-            unique = Math.floor(Math.random() * 999999)
-            
-            if(aqCheckbox.checked == true){
 
-                answerOutput += `
-        
-                    <div class="msi-radio">
-                        <span>${aqTiltle.value}</span>
-                        <input class="rrr" data-radio="textarea${unique}" type="radio" name="sec${count}">
-                    </div>
 
-                    `
-                textareaOutpot += `
-            
-                    <div class="msi-textarea none">
-                        <span class="block"> توضیحات درباره ${aqTiltle.value}</span>
-                        <textarea name="textarea${unique}" id=""></textarea>
-                    </div>
-                    
-            `
 
-            } else {
 
-                answerOutput += `
-        
-                    <div class="msi-radio">
-                        <span>${aqTiltle.value}</span>
-                        <input class="rrr" data-radio="" type="radio" name="sec${count}">
-                    </div>
 
-                    `
+
+
+        if (counter == aqTitles.length) {
+
+
+            let addFormData = new FormData(qForm)
+            let blosh = document.querySelectorAll('.blosh')
+            let quesId = Math.floor(Math.random() * 999999)
+            let aqInput = document.querySelector('#aq-input')
+            idCount++
+
+
+            let nre = {
+                "ques_id": quesId,
+                "title": aqInput.value,
+                "answers": []
             }
+
+            obj.items.push(nre)
+            let aqTitles = document.querySelectorAll('.aq-titles')
+
+
+            let ansId = 0
+            blosh.forEach(item => {
+                let aqTitle = item.querySelector('.aq-titles')
+                Math.floor(Math.random() * 999)
+
+                // obj.items[idCount].answers = []
+
+                // let array = []
+                let xxx = {
+                    title: aqTitle.value,
+                    ans_id: ansId
+                }
+                obj.items[idCount].answers.push(xxx)
+                // let ggg = obj.items.answers = array
+                // ggg.push(xxx)
+
+                // console.log(obj.items[idCount].answers)
+
+            })
+
+
+            let middleShow = document.querySelector('.middle-show')
+            middleShow.innerHTML = ''
             
 
-        })
+            // console.log(obj.items)
 
-        
-        let ansInputs = document.querySelectorAll('.aq-titles')
-        let showOutput  = '';
-        showOutput += `
-            
-        <div class="ms-item">
-            <div class="msi-title">
-                <span>${data.get('q_name')}</span>
-            </div>
-            <div class="msi-box">
-                <div class="msi-item">
-                    <div class="msi-ans">
+            obj.items.forEach(objItem => {
 
-                        ${answerOutput}
+                let outPut = ''
+                let secRadio = ''
 
+
+                objItem.answers.forEach(itemm => {
+
+
+                    secRadio += `
+                    
+                    <div class="msi-radio">
+                        <span>${itemm.title}</span>
+                        <input class="rrr" data-f="بله" type="radio" name="one">
                     </div>
-                     ${textareaOutpot}
-                </div>
-            </div>
-        </div>
+                    `
 
-            `
-
-        let middleShow = document.querySelector('.middle-show')
-            middleShow.insertAdjacentHTML('beforeend', showOutput)
+                })
 
 
+                outPut = `
+                    <div class="ms-item">
+                        <div class="msi-title">
+                            <span>${objItem.title}</span>
+                        </div>
+                        <div class="msi-box">
+                            <div class="msi-item">
+                                <div class="msi-ans">
+                                    ${secRadio}
+                                </div>
+                                <div class="msi-textarea">
+                                    <span class="block">توضیحات</span>
+                                    <textarea name="" id=""></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `
 
-        qForm.reset()
-        // tr.forEach(item => {
 
-        //     item.remove();
+                middleShow.insertAdjacentHTML('beforeend', outPut)
+
+
+
+
+
+
+
+
+
+            })
+
+            // let outPut = ''
+
+
+            // let middleShow = document.querySelector('.middle-show')
+
+
+            // middleShow.innerHTML = ''
+
+
+            // // radioSec += `
+
+            // // <div class="msi-radio">
+            // //     <span>بلی</span>
+            // //     <input class="rrr" data-f="بله" type="radio" name="one">
+            // // </div>
+
+            // // `
+
+
+
+
+            // let radioSec = ''
+
+            // console.log(obj.items[idCount].answers[0].title)
+
+            // obj.items.forEach(item => {
+
+            //     // console.log(item.answers[0])
+            //     // console.log(item.answers)
+
+            //     item.answers.forEach(itemm => {
+            //         // console.log(itemm.title)
+
+            //         radioSec += `
+
+            //         <div class="msi-radio">
+            //             <span>${itemm.title}</span>
+            //             <input class="rrr" data-f="بله" type="radio" name="one">
+            //         </div>
+
+            // `
+            //     })
+
+            //     if(item.title !== null){
+            //         outPut = `
+            //         <div class="ms-item">
+            //             <div class="msi-title">
+            //                 <span>${item.title}</span>
+            //             </div>
+            //             <div class="msi-box">
+            //                 <div class="msi-item">
+            //                     <div class="msi-ans">
+            //                         ${radioSec}
+            //                     </div>
+            //                     <div class="msi-textarea">
+            //                         <span class="block">توضیحات</span>
+            //                         <textarea name="" id=""></textarea>
+            //                     </div>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //         `
+
+
+            //         middleShow.insertAdjacentHTML('beforeend', outPut)
+            //     }
+
+
+            // })
+
+
+
+
+
+        } else {
+            alert('عنوان پاسخ ها نباید خالی باشند ')
+        }
+
+
+
+
+
+
+        // aqTitles.forEach(item => {
+
+        //     if(item.value !== ''){
+        //         isValid = true;
+        //     } else {
+        //         isValid = false;
+        //         return;
+        //     }
+
+        //     console.log(item)
+
+        // });
+
+
+
+
+        // if(isValid == true){
+        //     console.log('true')
+        // } else {
+        //     console.log('false')
+        // }
+
+
+
+        // aqTitles.forEach(item => {
+        //     console.log(item.value)
         // })
+
 
     } else {
         alert('عنوان اصلی نباید خالی باشد')
     }
 
 
-    let checkShow = document.querySelectorAll('.rrr[data-radio]')
-    if(checkShow){
 
-        checkShow.forEach(item =>{
-            item.addEventListener('change' , function(){
-                
-                if(item.getAttribute('data-radio') !== ''){
-                    
-                    let dataRadio = item.getAttribute('data-radio');
-                    let tArea = document.querySelector(`[name=${dataRadio}]`)
-                    let msiItem = tArea.parentNode.parentNode;
-                    let msiItems = msiItem.querySelectorAll('.msi-textarea')
+    // if (aqInput.value !== '') {
 
-                    tArea.parentNode.classList.remove('none')
+    //     let data = new FormData(Qform)
+    //     let blosh = document.querySelectorAll('.blosh')
+    //     let aqTitles = document.querySelectorAll('.aq-titles')
+    //     count++
 
-                    msiItems.forEach(item => {
-                        item.classList.add('none')
-                    })
+    //     let answerOutput = ''
+    //     let textareaOutpot = ''
 
-                    tArea.parentNode.classList.remove('none')
+    //     blosh.forEach(item => {
+    //         let aqTiltle = item.querySelector('.aq-titles')
+    //         let aqCheckbox = item.querySelector('.checkbox')
 
-                    // console.log()
-                } else {
-                    
-                    let dataradioOff = item.parentNode.parentNode.parentNode
-                    let makeNone = dataradioOff.querySelectorAll('.msi-textarea')
+    //         unique = Math.floor(Math.random() * 999999)
 
-                    makeNone.forEach(tItem => {
+    //         if(aqCheckbox.checked == true){
 
-                        tItem.classList.add('none')
-                        tItem.childNodes[3].value = ''
-                    })
-                }
-            })
-        })
-    }
+    //             answerOutput += `
+
+    //                 <div class="msi-radio">
+    //                     <span>${aqTiltle.value}</span>
+    //                     <input class="rrr" data-radio="textarea${unique}" type="radio" name="sec${count}">
+    //                 </div>
+
+    //                 `
+    //             textareaOutpot += `
+
+    //                 <div class="msi-textarea none">
+    //                     <span class="block"> توضیحات درباره ${aqTiltle.value}</span>
+    //                     <textarea name="textarea${unique}" id=""></textarea>
+    //                 </div>
+
+    //         `
+
+    //         } else {
+
+    //             answerOutput += `
+
+    //                 <div class="msi-radio">
+    //                     <span>${aqTiltle.value}</span>
+    //                     <input class="rrr" data-radio="" type="radio" name="sec${count}">
+    //                 </div>
+
+    //                 `
+    //         }
+
+
+    //     })
+
+
+    //     let ansInputs = document.querySelectorAll('.aq-titles')
+    //     let showOutput  = '';
+    //     showOutput += `
+
+    //     <div class="ms-item">
+    //         <div class="msi-title">
+    //             <span>${data.get('q_name')}</span>
+    //         </div>
+    //         <div class="msi-box">
+    //             <div class="msi-item">
+    //                 <div class="msi-ans">
+
+    //                     ${answerOutput}
+
+    //                 </div>
+    //                  ${textareaOutpot}
+    //             </div>
+    //         </div>
+    //     </div>
+
+    //         `
+
+    //     let middleShow = document.querySelector('.middle-show')
+    //         middleShow.insertAdjacentHTML('beforeend', showOutput)
+
+
+
+    //     qForm.reset()
+    //     // tr.forEach(item => {
+
+    //     //     item.remove();
+    //     // })
+
+    // } else {
+    //     alert('عنوان اصلی نباید خالی باشد')
+    // }
+
+
+    // let checkShow = document.querySelectorAll('.rrr[data-radio]')
+    // if(checkShow){
+
+    //     checkShow.forEach(item =>{
+    //         item.addEventListener('change' , function(){
+
+    //             if(item.getAttribute('data-radio') !== ''){
+
+    //                 let dataRadio = item.getAttribute('data-radio');
+    //                 let tArea = document.querySelector(`[name=${dataRadio}]`)
+    //                 let msiItem = tArea.parentNode.parentNode;
+    //                 let msiItems = msiItem.querySelectorAll('.msi-textarea')
+
+    //                 tArea.parentNode.classList.remove('none')
+
+    //                 msiItems.forEach(item => {
+    //                     item.classList.add('none')
+    //                 })
+
+    //                 tArea.parentNode.classList.remove('none')
+
+    //                 // console.log()
+    //             } else {
+
+    //                 let dataradioOff = item.parentNode.parentNode.parentNode
+    //                 let makeNone = dataradioOff.querySelectorAll('.msi-textarea')
+
+    //                 makeNone.forEach(tItem => {
+
+    //                     tItem.classList.add('none')
+    //                     tItem.childNodes[3].value = ''
+    //                 })
+    //             }
+    //         })
+    //     })
+    // }
 })
 
 
 
 
 
-function alert(text){
-    
+function alert(text) {
+
     let alert = document.querySelector('.alert')
 
     alert.style.animation = 'alert alternate forwards 1s ';
-    alert.childNodes[1] .innerHTML = text
+    alert.childNodes[1].innerHTML = text
 
 
     setTimeout(() => {
         // alert.classList.add('none')
         alert.style.animation = 'alertClear alternate backwards 1s ';
-        alert.childNodes[1] .innerHTML = ''
+        alert.childNodes[1].innerHTML = ''
     }, 3000);
 }
 
 
+function dateTime() {
+    let currentdate = new Date();
+    let datetime = currentdate.getDate() + "/"
+        + (currentdate.getMonth() + 1) + "/"
+        + currentdate.getFullYear() + " @ "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds();
+
+    return datetime
+}
+
+
+
+
+
+
+// let xxx = {
+//     "Items" : '111',
+// }
+
+
+
+// var Items = JSON.parse(localStorage.getItem("Items"))
+// var JSONItems = localStorage.setItem("Items", JSON.stringify(xxx))
+
+// console.log(Items)
 
 
---
+++ json.js
@@ -0,0 +1,319 @@
+let Obyj = {
+    "id": "",
+    "created_time": "",
+    "items": [
+        {
+            "ques_id" : null,
+            "show": true,
+            "title": "مشتری پاسخگوی تماس بوده است؟",
+            "type": "radio",
+            "answers": [
+                {
+                    "ans_id": '',
+                    "title" : '',
+                    "child": [],
+                    "default": [],
+                    "descrip": false,
+                    "desctext" : "",
+                    "name": '',
+                }
+            ],
+            "ques_can_select": [
+
+            ]
+
+        },
+    ]
+}
+
+let obdrgdrj = {
+    "_id": "634e32f5ac6f7daf3ce60b4f",
+    "code": "51",
+    "created_at": "2022-10-18T08:30:37.285+03:30",
+    "form_type": "اطلاع رسانی تنظیم بازار",
+    "id": "634e32f5ac6f7daf3ce60b4f",
+    "map": [
+        {
+            "answers": [
+                {
+                    "childs": [
+                        1
+                    ],
+                    "default": [
+                        {
+                            "key": " سوال1",
+                            "value": 1
+                        }
+                    ],
+                    "description": false,
+                    "description_content": "",
+                    "file": "",
+                    "id": 0,
+                    "name": "a0",
+                    "removeAllSelected": false,
+                    "selected": false,
+                    "title": "بلی"
+                },
+                {
+                    "childs": [],
+                    "default": [],
+                    "description": false,
+                    "description_content": "",
+                    "file": "",
+                    "id": 1,
+                    "name": "a1",
+                    "removeAllSelected": false,
+                    "selected": false,
+                    "title": "خیر"
+                }
+            ],
+            "id": 0,
+            "isAnswered": false,
+            "isChild": false,
+            "name": "q0",
+            "order": 1,
+            "ques_can_select": [
+                {
+                    "key": " سوال1",
+                    "value": 1
+                },
+                {
+                    "key": " سوال2",
+                    "value": 2
+                },
+                {
+                    "key": " سوال3",
+                    "value": 3
+                },
+                {
+                    "key": " سوال4",
+                    "value": 4
+                }
+            ],
+            "show": true,
+            "title": "مشتری پاسخگوی تماس بوده است؟",
+            "type": "radio"
+        },
+        {
+            "answers": [
+                {
+                    "childs": [
+                        2
+                    ],
+                    "default": [
+                        {
+                            "key": " سوال2",
+                            "value": 2
+                        }
+                    ],
+                    "description": false,
+                    "description_content": "",
+                    "file": "",
+                    "id": 0,
+                    "name": "a0",
+                    "removeAllSelected": false,
+                    "selected": false,
+                    "title": "بلی"
+                },
+                {
+                    "childs": [
+                        2
+                    ],
+                    "default": [
+                        {
+                            "key": " سوال2",
+                            "value": 2
+                        }
+                    ],
+                    "description": false,
+                    "description_content": "",
+                    "file": "",
+                    "id": 1,
+                    "name": "a1",
+                    "removeAllSelected": false,
+                    "selected": false,
+                    "title": "خیر"
+                }
+            ],
+            "id": 1,
+            "isAnswered": false,
+            "isChild": true,
+            "name": "q1",
+            "order": 2,
+            "ques_can_select": [
+                {
+                    "key": " سوال2",
+                    "value": 2
+                },
+                {
+                    "key": " سوال3",
+                    "value": 3
+                },
+                {
+                    "key": " سوال4",
+                    "value": 4
+                }
+            ],
+            "show": false,
+            "title": "بازرس جهت آموزش مراجعه نمودند؟",
+            "type": "radio"
+        },
+        {
+            "answers": [
+                {
+                    "childs": [
+                        3
+                    ],
+                    "default": [
+                        {
+                            "key": " سوال3",
+                            "value": 3
+                        }
+                    ],
+                    "description": false,
+                    "description_content": "",
+                    "file": "",
+                    "id": 0,
+                    "name": "a0",
+                    "removeAllSelected": false,
+                    "selected": false,
+                    "title": "بلی"
+                },
+                {
+                    "childs": [
+                        3
+                    ],
+                    "default": [
+                        {
+                            "key": " سوال3",
+                            "value": 3
+                        }
+                    ],
+                    "description": false,
+                    "description_content": "",
+                    "file": "",
+                    "id": 1,
+                    "name": "a1",
+                    "removeAllSelected": false,
+                    "selected": false,
+                    "title": "خیر"
+                }
+            ],
+            "id": 2,
+            "isAnswered": false,
+            "isChild": true,
+            "name": "q2",
+            "order": 3,
+            "ques_can_select": [
+                {
+                    "key": " سوال3",
+                    "value": 3
+                },
+                {
+                    "key": " سوال4",
+                    "value": 4
+                }
+            ],
+            "show": false,
+            "title": "اطلاعات آموزشی به همراه بروشور در اختیارتان گذاشته شد؟",
+            "type": "radio"
+        },
+        {
+            "answers": [
+                {
+                    "childs": [
+                        4
+                    ],
+                    "default": [],
+                    "description": false,
+                    "description_content": "",
+                    "id": 0,
+                    "name": "a0",
+                    "selected": false,
+                    "title": "بلی"
+                },
+                {
+                    "childs": [
+                        4
+                    ],
+                    "default": [],
+                    "description": true,
+                    "description_content": "",
+                    "id": 1,
+                    "name": "a1",
+                    "selected": false,
+                    "title": "خیر"
+                }
+            ],
+            "id": 3,
+            "isAnswered": false,
+            "isChild": true,
+            "name": "q3",
+            "ques_can_select": [
+                {
+                    "key": " سوال4",
+                    "value": 4
+                }
+            ],
+            "show": false,
+            "title": "آموزش توسط همکاران پیشخوان داده شد؟",
+            "type": "radio"
+        },
+        {
+            "answers": [
+                {
+                    "childs": [],
+                    "default": [],
+                    "description": true,
+                    "description_content": "",
+                    "file": "",
+                    "id": 0,
+                    "name": "a0",
+                    "selected": false,
+                    "title": "بلی"
+                },
+                {
+                    "childs": [],
+                    "default": [],
+                    "description": false,
+                    "description_content": "",
+                    "file": "",
+                    "id": 1,
+                    "name": "a1",
+                    "selected": false,
+                    "title": "خیر"
+                }
+            ],
+            "id": 4,
+            "isAnswered": false,
+            "isChild": true,
+            "name": "q4",
+            "ques_can_select": [],
+            "show": false,
+            "title": "سوال و یا ابهامی درخصوص طرح دارید",
+            "type": "radio"
+        }
+    ]
+}
+
+
+console.log(obdrgdrj)
+
+
+
+
+let text = { "employees" : [            
+                                { "firstName":"John" , "lastName":"Doe" },
+                                { "firstName":"Anna" , "lastName":"Smith" }, 
+                                { "firstName":"Peter" , "lastName":"Jones" }
+ ]}; 
+
+            // let object = JSON.parse(text)
+
+// console.log(object)
+
+
+var JSONObj = new Object(text)
+
+// console.log(JSONObj)
+
